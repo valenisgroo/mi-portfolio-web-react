c41b3204afc86f321fea0b810e968505f4871060
valentino <valentinoisgro@gmail.com>
2024-07-19 02:17:59 -0300

    Se creó la carpeta Components y además se trabajó el Header

---
+++ PortfolioWeb/src/Components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css' 
+
+const Footer = () => {
+  return (
+    <div>
+      {/* Contenido del componente */}
+    </div>
+  );
+}
+
+export default Footer;
--- PortfolioWeb/src/main.jsx
+++ PortfolioWeb/src/main.jsx
@@ -3,8 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import Header from './Components/Header/Header.jsx'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Header />
     <App />
+    <Header />
+    <Header />
   </React.StrictMode>,
 )
